fd9bd82adf0957c48f92ff1fe3d9033845da0816
Gabriel Amorim <gabriel._amorim@hotmail.com>
2021-11-08 14:14:14 -0300

    salvando

---
+++ src/components/NewChat.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import './NewChat.css';
+
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+export default ({user,chatlist, show, setShow}) => {
+    const [list, setList] = useState([
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Amorim'},
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Amorim'},
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Amorim'},
+        {id: 123, avatar: 'https://www.w3schools.com/howto/img_avatar2.png', name: 'Amorim'}
+    ])
+
+    const handleClose = () => {
+         setShow(false);
+    }
+
+    return (
+        <div className="newChat" style={{left: show?0:-415}}>
+            <div className="newChat--head">
+                <div onClick={handleClose} className="newChat--backButton">
+                    <ArrowBackIcon style={{color: '#FFF'}} />
+                </div>
+                <div className="newChat--headtitle">Nova Conversa</div>
+            </div>
+            <div className="newChat--list">
+                {list.map((item, key)=>(
+                    <div className="newChat--item" key={key}>
+                        <img className="newChat--itemavatar" src={item.avatar} alt="" />
+                        <div className="newChat--itemname">{item.name}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
